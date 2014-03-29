9f6a4417808661412e122e869f2efbd8a67fda53
henge <henggge@gmail.com>
2014-03-29 13:20:07 +0900

    フォームによる値の送信

--- src/main/java/jp/springbook/springmyapp/MyAppController.java
+++ src/main/java/jp/springbook/springmyapp/MyAppController.java
@@ -2,6 +2,7 @@ package jp.springbook.springmyapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +11,16 @@ public class MyAppController {
 
 	@RequestMapping(value = "/helo", method = RequestMethod.GET)
 	public String helo(Model model) {
-		model.addAttribute("message", "this is sample. ok?");
+		FormModel fm = new FormModel();
+		fm.setInput1("ここに書く");
+		model.addAttribute("formModel", fm);
+		model.addAttribute("message", "何か書いてください。");
+		return "showMessage";
+	}
+	
+	@RequestMapping(value = "/helo", method = RequestMethod.POST)
+	public String form(@ModelAttribute FormModel fm, Model model) {
+		model.addAttribute("message", "you typed: " + fm.getInput1());
 		return "showMessage";
 	}
 }
--- src/main/webapp/WEB-INF/view/showMessage.jsp
+++ src/main/webapp/WEB-INF/view/showMessage.jsp
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-    
+
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Welcome</title>
-	</head> 
-	<body>
-		<h2>${message}</h2>
-	</body>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h2>${message}</h2>
+  
+    <form:form modelAttribute="formModel">
+      <form:input path="input1" />
+      <input type="submit">
+    </form:form>
+  </body>
 </html>
